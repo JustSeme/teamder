89ebab0590f646777d1b9f5957c27dcd82699c41
Rodion Strelkov <rodionstrelkov@Rodions-MacBook-Air.local>
2022-06-25 12:40:17 +0600

    feat: упаковка данных в state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './components/Home/Home';
-import Profile from './components/Profile/Profile';
-import Popup from './components/Popup/Popup';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
+import Popup from "./components/Popup/Popup";
 import Messages from "./components/Messages/Messages";
 
 function App(props) {
-
   return (
     <div className="page">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} exact />
           <Route path="/messages" element={<Messages />} exact />
-          <Route path="/profile" element={<Profile profilePosts={props.profilePosts} myPosts={props.myPosts} />} exact/>
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                profilePosts={props.state.profilePage.profilePosts}
+                myPosts={props.state.profilePage.myPosts}
+              />
+            }
+            exact
+          />
         </Routes>
       </BrowserRouter>
     </div>
